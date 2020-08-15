de0e9a86f21e035b46ef73a2f56b3eda606a4a3c
Brian Zhang <bbrianzzhang@gmail.com>
2020-08-15 00:43:28 -0700

    fix dashboard display

--- flaskr/dashboard.py
+++ flaskr/dashboard.py
@@ -17,7 +17,7 @@ def index():
         " FROM post p JOIN user u ON p.author_id = u.id"
         " ORDER BY created DESC"
     ).fetchall()
-    return render_template("dashboard/index.html", posts=posts)
+    return render_template("dashboard/test.html", posts=posts)
 
 
 @bp.route("/create", methods=("GET", "POST"))
